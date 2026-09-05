882c769b3d9de885c9221fe56cda44e97a4ef14b
Rafael Marques de Souza <rafael@Rafaels-Air.fritz.box>
2026-09-05 23:24:34 +0200

    Add counter, shuffle songs and play whole list of songs.

--- tocador.py
+++ tocador.py
@@ -6,11 +6,15 @@ import random
 
 folder = Path(__file__).parent / 'music'
 library = list(folder.glob('*.mp3'))
+random.shuffle(library)
+number_of_songs=len(library)
 
-if len(library) == 0:
+if number_of_songs == 0:
     print('Sorry, no songs available in the folder music :(')
 else:
-    song = random.choice(library)
-    print(f'Playing {song.stem.replace('_',' ')} ...')
-    playsound(song)
+    for song in library:
+        counter=int(1)
+        print(f'\nPlaying {counter} of {number_of_songs} ...\n{song.stem.replace('_',' ')}')
+        playsound(song)
+        counter=counter+1
     print('End.')
