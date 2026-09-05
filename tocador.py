85a1254ff7aa32d52c7d004d1a2fb428eadce88c
Rafael Marques de Souza <rafael@Rafaels-Air.fritz.box>
2026-09-05 15:44:41 +0200

    Add message if folder is empaty and replace underscores.

--- tocador.py
+++ tocador.py
@@ -4,10 +4,13 @@ from pathlib import Path
 from playsound3 import playsound
 import random
 
-folder=Path(__file__).parent / 'music'
-library=list(folder.glob('*.mp3'))
-song=random.choice(library)
+folder = Path(__file__).parent / 'music'
+library = list(folder.glob('*.mp3'))
 
-print(f'Playing {song.stem} ...')
-playsound(song)
-print('End.')
+if len(library) == 0:
+    print('Sorry, no songs available in the folder music :(')
+else:
+    song = random.choice(library)
+    print(f'Playing {song.stem.replace('_',' ')} ...')
+    playsound(song)
+    print('End.')
