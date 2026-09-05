6be25b257fef8c0d7da510d7c2094154c582068f
Rafael Marques de Souza <rafael@Rafaels-Air.fritz.box>
2026-09-06 00:02:14 +0200

    Show track position, replace manual counter by function enumarate

--- tocador.py
+++ tocador.py
@@ -7,14 +7,12 @@ import random
 folder = Path(__file__).parent / 'music'
 library = list(folder.glob('*.mp3'))
 random.shuffle(library)
-number_of_songs=len(library)
+number_of_songs = len(library)
 
 if number_of_songs == 0:
     print('Sorry, no songs available in the folder music :(')
 else:
-    for song in library:
-        counter=int(1)
-        print(f'\nPlaying {counter} of {number_of_songs} ...\n{song.stem.replace('_',' ')}')
+    for position, song in enumerate(library, start=1):
+        print(f'\nPlaying {position} of {number_of_songs} ...\n{song.stem.replace('_',' ')}')
         playsound(song)
-        counter=counter+1
     print('End.')
